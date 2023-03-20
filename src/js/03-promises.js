61d6b149f89bb0aa476c656a3a10ada9f7648077
HelenBlyzniuk <helenblyzniuk2@gmail.com>
2023-03-20 17:39:15 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
+
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('input[name="delay"]');
+const stepEl = document.querySelector('input[name="step"]');
+console.log(stepEl)
+const amountEl = document.querySelector('input[name="amount"]');
+// console.log(delayEl.value);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve (`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    reject(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+
+// function callPromise(amount, step) {
+  
+//   for (let i = 0, i <= amount, i ++){
+//     createPromise(i, delay);
+//   }
+// }
+let delay = null;
+let step = null;
+let amount = null;
+
+delayEl.addEventListener('input', (e) => {
+  e.preventDefault;
+  delay = e.currentTarget.value;
+  console.log(delay)
+  return delay;
+});
+
+stepEl.addEventListener('input', (e) => {
+  e.preventDefault;
+  step = e.currentTarget.value;
+  console.log(step);
+  return step;
+});
+
+amountEl.addEventListener('input', (e) => {
+  e.preventDefault;
+  amount = e.currentTarget.value;
+    console.log(amount);
+  return amount;
+
+});
+
+
+
